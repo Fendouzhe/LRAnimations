66030d8cb8c3c30d5b674dc811a333e6113279ff
LLR <627987140@qq.com>
2018-01-01 13:33:19 +0800

    68

---
+++ LRAnimations/LRAnimations/Controller/LRUnlockAnimationController.h
@@ -0,0 +1,13 @@
+//
+//  LRUnlockAnimationController.h
+//  LRAnimations
+//
+//  Created by 雷路荣 on 2018/1/1.
+//  Copyright © 2018年 LeiLuRong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LRUnlockAnimationController : UIViewController
+
+@end
---
+++ LRAnimations/LRAnimations/Controller/LRUnlockAnimationController.m
@@ -0,0 +1,37 @@
+//
+//  LRUnlockAnimationController.m
+//  LRAnimations
+//
+//  Created by 雷路荣 on 2018/1/1.
+//  Copyright © 2018年 LeiLuRong. All rights reserved.
+//
+
+#import "LRUnlockAnimationController.h"
+
+@interface LRUnlockAnimationController ()
+
+@end
+
+@implementation LRUnlockAnimationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
